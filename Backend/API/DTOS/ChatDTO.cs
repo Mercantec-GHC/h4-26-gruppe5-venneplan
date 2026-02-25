3f60bcf9a68a943551d6e2cbc6d973b5ef32f6cf
Elic0 <charlieoljac@gmail.com>
2026-02-25 08:23:07 +0100

    Added chat migration

---
+++ Backend/API/DTOS/ChatDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace API.DTOS
+{
+    public class SendDto { 
+        public int SenderId { get; set; } = 0; 
+        public int ReceiverId { get; set; } = 0; 
+        public string Content { get; set; } = ""; 
+    }
+}
---
+++ Backend/API/Models/ChatMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Models
+{
+    public class ChatMessage
+    {
+        public int Id { get; set; }
+        public int SenderId { get; set; }
+        public int ReceiverId { get; set; }
+        public required string Content { get; set; }
+        public DateTime SentAt { get; set; } = DateTime.UtcNow;
+    }
+}
